a6ed8b25ec9f85dad6bb86aafc3c781fb354aa49
Bertrand Sim <bertrand.sim@users.noreply.github.com>
2020-09-05 14:22:17 +0800

    Steno layer working; Gemini protocol
    Currently using only Gemini protocol. (see qmk steno docs for how to switch
    between both on the fly)
    
    Currently, main steno keys on bottom two rows of kb matrix.
    
    in rules.mk, enabled STENO_ENABLE, NKRO_ENABLE.
    disabled BACKLIGHT_ENABLE (for single color leds)
    to reduce firmware space

--- keyboards/keebio/iris/keymaps/BertrandSim/keymap.c
+++ keyboards/keebio/iris/keymaps/BertrandSim/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include <keymap_steno.h>
 
 
 #define _QWERTY 0
@@ -33,15 +34,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_STENO] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_S,    KC_T,    KC_P,    KC_H,    KC_ASTR,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+     STN_PWR, STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,                             STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_S,    KC_K,    KC_W,    KC_R,    KC_ASTR,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,                            STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_RE1,          STN_RE2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR ,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    LOWER,   KC_ENT,  KC_LGUI,                   KC_RALT, KC_SPC,  RAISE
+                                    LOWER,   STN_A,   STN_O,                     STN_E,   STN_U,   RAISE
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -79,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, RESET,   _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, QWERTY,  STENO,   _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,                            _______, QWERTY,  DF(_STENO), _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -88,17 +89,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+void matrix_init_user() {
+  // use only Gemini PR Steno protocol
+  steno_set_mode(STENO_MODE_GEMINI); // or STENO_MODE_BOLT
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case STENO:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_STENO);
       }
       return false;
       break;
--- keyboards/keebio/iris/keymaps/BertrandSim/rules.mk
+++ keyboards/keebio/iris/keymaps/BertrandSim/rules.mk
@@ -21,15 +21,17 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # keyboard backlight functionality (single color leds)
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+BLUETOOTH_ENABLE = no       # Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE = yes       # WS2812 RGB underlight.
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+STENO_ENABLE = yes          # Stenotype 
 
 SPLIT_KEYBOARD = yes
 ENCODER_ENABLE = yes
